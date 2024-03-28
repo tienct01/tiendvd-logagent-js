c3daf175f3228dc9300fee5a97b5c67a0bccd1ec
SBC - TIENDVD <tiendvd@bsscommerce.com>
2024-03-29 00:59:32 +0700

    custom env for service

--- bin/logagent-windows.js
+++ bin/logagent-windows.js
@@ -5,15 +5,25 @@ try {
     var laCmd = process.mainModule.filename
     // var exec = require('child_process').execSync
     // exec('powershell -command "Set-ItemProperty -Path \'Registry::HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\Environment\' -Name LOGAGENT_CONFIG –Value \'' + process.argv[3] +'\'',{stdio: [0,1,2]})
-    console.log(laCmd)
     let Service = require('node-windows').Service
+    let logagentEnv = [];
+    if (process.argv[3] === '--skipDefaultPatterns') {
+      logagentEnv = {
+        name: "DISABLE_DEFAULT_PATTERNS",
+        value: "true"
+      }
+    }
     let svc = new Service({
       name: 'Logagent',
       description: 'Sematext Logagent',
       script: laCmd,
       wait: 2,
-      grow: 0.5
+      grow: 0.5,
+      env: logagentEnv,
     })
+    console.log("Service root: ", svc.root);
+    console.log("Service script path: ", laCmd);
+    console.log("Skip default pattern: ", Object.keys(logagentEnv).length > 0);
     svc.on('error', console.log)
     // Listen for the "install" event, which indicates the
     // process is available as a service.
